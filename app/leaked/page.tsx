6791310518d919507d55cd2e67a78dd0147a7c34
syed abdul muqeeth <samx123786@gmail.com>
2025-08-23 17:18:04 +0530

    added static pages for assignments, cheat, jobs, leaked, projects, scholarships

---
+++ app/leaked/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from "react";
+
+export default function LeakedPage() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-4xl font-bold text-center">Jobs</h1>
+    </div>
+  );
+}
---
+++ app/scholarship/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import React from "react";
+export default function SharShipPage() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-4xl font-bold text-center">Scholarships</h1>
+    </div>
+  );
+}
