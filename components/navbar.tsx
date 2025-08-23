a9ba49bd0ee7a5ff6b2b0c002bd5978cf7951e5e
syed abdul muqeeth <samx123786@gmail.com>
2025-08-23 17:00:34 +0530

    removed search bar and theme switcher

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,54 +2,17 @@ import {
   Navbar as HeroUINavbar,
   NavbarContent,
   NavbarBrand,
-  NavbarItem,
 } from "@heroui/navbar";
-import { Kbd } from "@heroui/kbd";
-import { Input } from "@heroui/input";
 import NextLink from "next/link";
-
-import { ThemeSwitch } from "@/components/theme-switch";
-import { SearchIcon } from "@/components/icons";
-
 export const Navbar = () => {
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      labelPlacement="outside"
-      placeholder="Enter USN"
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
-  );
-
   return (
-    <HeroUINavbar maxWidth="xl" position="sticky">
+    <HeroUINavbar className="my-2" position="sticky" maxWidth="xl">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
             <p className="font-bold text-xl md:text-2xl lg:text-4xl text-inherit">Exam.Sucks</p>
           </NextLink>
         </NavbarBrand>
-      </NavbarContent>
-
-      <NavbarContent
-        className="hidden sm:flex basis-1/5 sm:basis-full"
-        justify="end"
-      >
-        <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
-        <NavbarItem className="hidden sm:flex gap-2">
-          <ThemeSwitch />
-        </NavbarItem>
-      </NavbarContent>
-
-      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-      <NavbarItem>{searchInput}</NavbarItem>
       </NavbarContent>
     </HeroUINavbar>
   );
