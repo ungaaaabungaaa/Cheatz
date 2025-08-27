b9d6755a45743f86a3a23fdb25c72f7b0f51dee9
syed abdul muqeeth <samx123786@gmail.com>
2025-08-27 15:28:06 +0530

    fixed scholarship page

--- app/scholarship/page.tsx
+++ app/scholarship/page.tsx
@@ -8,7 +8,7 @@ export default function ScholarshipPage() {
     <section className="mx-auto w-full max-w-6xl py-20 sm:px-6 sm:py-32 lg:px-8 lg:py-40">
       <div className="mx-auto flex w-full max-w-4xl flex-col items-center gap-6 lg:flex-row lg:items-start lg:gap-12">
         <h2 className="px-2 text-3xl leading-7">
-          <span className="inline-block lg:hidden">FAQs</span>
+          <span className="inline-block lg:hidden">$chol@rship&apos;s</span>
           <span className="from-foreground-800 to-foreground-500 dark:to-foreground-200 hidden bg-linear-to-br bg-clip-text pt-4 text-5xl font-semibold tracking-tight text-transparent lg:inline-block">
             Scholarship
             <br />
@@ -17,6 +17,7 @@ export default function ScholarshipPage() {
              Links!
           </span>
         </h2>
+
         <Accordion
           fullWidth
           keepContentMounted
